5491d6846b4e136066160c30f3f66b52924e39c4
m4cx <macgyverx83@gmail.com>
2020-07-12 14:24:39 +0200

    docs(sample): publish sample application on gh-pages

--- src/projects/octopatch-editor/src/app/app-routing.module.ts
+++ src/projects/octopatch-editor/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-
 const routes: Routes = [
-  {path: "editor", loadChildren: () => import("./editor/editor.module").then(m => m.EditorModule)}
+  {
+    path: "",
+    loadChildren: () =>
+      import("./editor/editor.module").then((m) => m.EditorModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
